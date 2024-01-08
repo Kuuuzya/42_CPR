ec5356ccb6c3003f7ce2aab1c7e342602cdd78a2
Sergey Kuznetsov <kuuuzya@ya.ru>
2024-01-09 02:24:11 +0400

    Initial commit

---
+++ Untitled-1.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+void ft_print_numbers(void)
+{
+    char c;
+    c = '0';
+    while (c <= '9')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+}
+
+int main(void)
+{
+    ft_print_numbers();
+    return (0);
+}
+
