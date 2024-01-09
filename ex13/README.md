a8c70ea6e5f513ea8e81bf6b23bd2c9d57dbd694
Sergey Kuznetsov <kuuuzya@ya.ru>
2024-01-09 17:49:40 +0400

    Added ex13

---
+++ ex13/README.md
@@ -0,0 +1,9 @@
+# Exercise 13 : ft_recursive_factorial
+<br />Files to turn in : ft_recursive_factorial.c
+<br />Turn-in directory : ex13/ 
+<br />Allowed functions : None
+<br /><br />
+- Create a recursive function that returns the factorial of the number given as a parameter.
+- If there’s an error, the function should return 0. 
+- Here’s how it should be prototyped :
+<br />`int ft_recursive_factorial(int nb);`
---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuznets <skuznets@student.42yerevan.am    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 17:38:48 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/09 17:49:31 by skuznets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_recursive_factorial(int nb)
+{
+	if (nb < 0 || nb > 12)
+		return 0;
+	if (nb == 0)
+		return 1;
+	else
+		return nb * ft_recursive_factorial(nb - 1);
+}
+/*
+#include <stdio.h>
+int		main(int argc, char **argv)
+{
+	int		nb;
+
+	nb = argv[1][0] - '0';
+	printf("%d", ft_recursive_factorial(nb));
+	return (0);
+}
+*/
