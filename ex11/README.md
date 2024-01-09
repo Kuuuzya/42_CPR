1d16950f5cfbf23bb879b1d984c6f5de4979e9c4
Sergey Kuznetsov <kuuuzya@ya.ru>
2024-01-09 13:04:34 +0400

    Added ex11

---
+++ ex11/README.md
@@ -0,0 +1,8 @@
+# Exercise 11 : ft_div_mod
+<br />Turn-in directory : ex11/
+<br />Files to turn in : ft_div_mod.c 
+<br />Allowed functions : None
+<br />
+- Create a function ft_div_mod prototyped like this :
+<br /> `void ft_div_mod(int a, int b, int *div, int *mod);`
+- This function divides parameters a by b and stores the result in the int pointed by div. It also stores the remainder of the division of a by b in the int pointed by mod.
---
+++ ex11/ft_div_mod.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuznets <skuznets@student.42yerevan.am    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 13:03:10 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/09 13:03:50 by skuznets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/*int main(void)
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 10;
+	b = 5;
+	ft_div_mod(a, b, &div, &mod);
+	write(1, &div, 1);
+	write(1, &mod, 1);
+	return (0);
+}*/
