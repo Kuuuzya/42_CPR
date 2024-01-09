ac1665209b02750bce7cb2825e6c3a7633d56b66
Sergey Kuznetsov <kuuuzya@ya.ru>
2024-01-09 13:08:47 +0400

    Added ex11

---
+++ ex12/README.md
@@ -0,0 +1,10 @@
+# Exercise 12 : ft_iterative_factorial
+<br />Files to turn in : ft_iterative_factorial.c
+<br />Turn-in directory : ex12/
+<br />Allowed functions : None
+<br /><br />
+- Create an iterated function that returns a number. This number is the result of a factorial operation based on the number given as a parameter.
+- If there’s an error, the function should return 0.
+- Here’s how it should be prototyped :
+<br />`int ft_iterative_factorial(int nb);`
+- Your function must return its result in less than two seconds.
---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuznets <skuznets@student.42yerevan.am    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 13:08:19 by skuznets          #+#    #+#             */
+/*   Updated: 2024/01/09 13:08:42 by skuznets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_iterative_factorial(int nb)
+{
+	int		i;
+	int		fact;
+
+	i = 1;
+	fact = 1;
+	if (nb < 0 || nb > 12)
+		return (0);
+	while (i <= nb)
+	{
+		fact *= i;
+		i++;
+	}
+	return (fact);
+}
+/*int main(void)
+{
+	int		nb;
+	int		fact;
+
+	nb = 5;
+	fact = ft_iterative_factorial(nb);
+	write(1, &fact, 1);
+	return (0);
+}*/
